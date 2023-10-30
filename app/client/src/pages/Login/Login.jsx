237953cb6062deaafa43c0e0a9176215a95e45b9
tmg-repo <tmg.01@icloud.com>
2023-10-30 16:32:29 -0700

    Validation added to login + register

--- app/client/src/pages/Login/Login.jsx
+++ app/client/src/pages/Login/Login.jsx
@@ -1,29 +1,87 @@
-import "./Login.scss"
+import React, { useState } from 'react';
+
+import './Login.scss';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    emailOrUsername: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState({
+    emailOrUsername: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newErrors = { ...errors };
+
+    // Basic validation checks
+    if (formData.emailOrUsername === '') {
+      newErrors.emailOrUsername = 'Email or username is required';
+    } else {
+      newErrors.emailOrUsername = '';
+    }
+
+    if (formData.password === '') {
+      newErrors.password = 'Password is required';
+    } else {
+      newErrors.password = '';
+    }
+
+    setErrors(newErrors);
+
+    // If there are no errors, you can submit the login form
+    if (!newErrors.emailOrUsername && !newErrors.password) {
+      // Your login submission logic here
+    }
+  };
+
   return (
     <div className="login">
-        <div className="card">
-            <div className="left">
-                <h1>EX-ZBT</h1>
-                <p>
-                    The purpose of the software is to provide a video sharing platform specifically designed for job interviews.
-                     It allows users to register, create profiles, upload and share videos for job interviews, and receive feedback/hiring decisions based on their video submissions.
-                </p>
-                <button>Register</button>
-            </div>
-            <div className="right">
-                <h1>Login</h1>
-                <form>
-                    <input type="text" placeholder="Email or username"/>
-                    <input type="password" placeholder="Password"/>
-                    <button>Login</button>
-                </form>
-
-            </div>
+      <div className="card">
+        <div className="left">
+          <h1>EX-ZBT</h1>
+          <p>
+            The purpose of the software is to provide a video sharing platform specifically designed for job interviews.
+            It allows users to register, create profiles, upload and share videos for job interviews, and receive feedback/hiring decisions based on their video submissions.
+          </p>
+          <button>Register</button>
         </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="emailOrUsername"
+              placeholder="Email or username"
+              value={formData.emailOrUsername}
+              onChange={handleChange}
+            />
+            <span className="error">{errors.emailOrUsername}</span>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <span className="error">{errors.password}</span>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- app/client/src/pages/Register/Register.jsx
+++ app/client/src/pages/Register/Register.jsx
@@ -5,7 +5,23 @@ import Login from '../Login/Login';
 const Register = () => {
   const [showLogin, setShowLogin] = useState(false);
   const [userType, setUserType] = useState('interviewee'); // Default to 'interviewee'
-  const [username, setUsername] = useState('');
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
 
   const toggleLogin = () => {
     setShowLogin(!showLogin);
@@ -15,8 +31,78 @@ const Register = () => {
     setUserType(event.target.value);
   };
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    // Basic validation to check for special symbols in first and last names
+    const specialSymbols = /[^A-Za-z\s-]/; // This regex allows letters, spaces, and hyphens
+
+    if (name === 'firstName' || name === 'lastName') {
+      if (specialSymbols.test(value)) {
+        setErrors({
+          ...errors,
+          [name]: 'Name should not contain special symbols',
+        });
+      } else {
+        setErrors({
+          ...errors,
+          [name]: '',
+        });
+      }
+    }
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newErrors = { ...errors };
+
+    // Basic validation checks
+    if (formData.firstName === '') {
+      newErrors.firstName = 'First Name is required';
+    }
+
+    if (formData.lastName === '') {
+      newErrors.lastName = 'Last Name is required';
+    }
+
+    if (formData.email === '') {
+      newErrors.email = 'Email is required';
+    } else if (!formData.email.match(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/)) {
+      newErrors.email = 'Invalid email format';
+    }
+
+    if (formData.username === '') {
+      newErrors.username = 'Username is required';
+    }
+
+    if (formData.password === '') {
+      newErrors.password = 'Password is required';
+    }
+
+    if (formData.confirmPassword === '') {
+      newErrors.confirmPassword = 'Confirm Password is required';
+    } else if (formData.confirmPassword !== formData.password) {
+      newErrors.confirmPassword = 'Passwords do not match';
+    }
+
+    setErrors(newErrors);
+
+    // If there are no errors, you can submit the registration form
+    if (
+      !newErrors.firstName &&
+      !newErrors.lastName &&
+      !newErrors.email &&
+      !newErrors.username &&
+      !newErrors.password &&
+      !newErrors.confirmPassword
+    ) {
+      // Your registration submission logic here
+    }
   };
 
   return (
@@ -31,22 +117,37 @@ const Register = () => {
           </div>
           <div className="right">
             <h1>Register</h1>
-            <form>
-              <input type="text" placeholder="Full Name" />
-              <input type="text" placeholder="Email" />
-              
+            <form onSubmit={handleSubmit}>
               <input
                 type="text"
-                placeholder="Username"
-                value={username}
-                onChange={handleUsernameChange}
+                name="firstName"
+                placeholder="First Name"
+                value={formData.firstName}
+                onChange={handleChange}
               />
-
+              <span className="error">{errors.firstName}</span>
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Last Name"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+              <span className="error">{errors.lastName}</span>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleChange}
+              />
+              <span className="error">{errors.username}</span>
               <div className="user-type">
                 <p>Select User Type:</p>
                 <label>
                   <input
                     type="radio"
+                    name="userType"
                     value="interviewee"
                     checked={userType === 'interviewee'}
                     onChange={handleUserTypeChange}
@@ -56,6 +157,7 @@ const Register = () => {
                 <label>
                   <input
                     type="radio"
+                    name="userType"
                     value="interviewer"
                     checked={userType === 'interviewer'}
                     onChange={handleUserTypeChange}
@@ -63,11 +165,31 @@ const Register = () => {
                   Interviewer
                 </label>
               </div>
-
-              <input type="password" placeholder="Password" />
-              <input type="password" placeholder="Confirm Password" />
-
-              <button>Register</button>
+              <input
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+              <span className="error">{errors.email}</span>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+              <span className="error">{errors.password}</span>
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+              />
+              <span className="error">{errors.confirmPassword}</span>
+              <button type="submit">Register</button>
             </form>
           </div>
         </div>
