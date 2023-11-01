53e638d8217f6263a4b3b402bfb484a303eed2ed
yemoski <yemisiogungbemi89@gmail.com>
2023-10-31 17:57:52 -0700

    does not exist

---
+++ app/client/src/pages/DoesNotExist/DoesNotExist.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./DoesNotExist.scss";
+
+
+function DoesNotExist() {
+  return (
+    <div className="container">
+      <div className="header">
+      <h2> Sorry, The Page you want to visit does not Exist :)</h2>
+      </div>
+
+    </div>
+
+  )
+}
+
+export default DoesNotExist;
