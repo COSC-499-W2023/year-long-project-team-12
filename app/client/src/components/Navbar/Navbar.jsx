71643f41d1c4ec5364ba159c2d8fba2373b698fb
yemoski <yemisiogungbemi89@gmail.com>
2023-10-31 18:15:54 -0700

    adding logo to navbar

--- app/client/src/components/Navbar/Navbar.jsx
+++ app/client/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import LoginIcon from '@mui/icons-material/Login';
 import SwitchAccountIcon from '@mui/icons-material/SwitchAccount';
+
+import logo from "./logo.png"
+
+
 import "./Navbar.scss";
 const Navbar = () => {
   return (
@@ -9,7 +13,7 @@ const Navbar = () => {
         <div className="wrapper">
             
             <div className='left'>
-               
+               <img src={logo} alt="logo"></img>
             </div>
             
             <div className='center'>
