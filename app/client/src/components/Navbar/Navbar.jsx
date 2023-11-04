93313585155e5eca48ff5feb312a566b39d720b8
yemoski <yemisiogungbemi89@gmail.com>
2023-11-03 23:56:21 -0700

    changes

--- app/client/src/components/Navbar/Navbar.jsx
+++ app/client/src/components/Navbar/Navbar.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import LoginIcon from '@mui/icons-material/Login';
 import SwitchAccountIcon from '@mui/icons-material/SwitchAccount';
+import WorkIcon from '@mui/icons-material/Work';
+import { useAuth } from "../../context/authContext"
 import logo from "./logo.png";
+import {redirect, useNavigate} from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = () => {
+    const { isCustomerAuthenticated,currentUser, logOut } = useAuth();
+    let navigate = useNavigate();
+
+    const handleLogout = (event) => {
+        logOut()
+        navigate("/home");
+    };
+    console.log(currentUser);
+    console.log(isCustomerAuthenticated());
   return (
+    <div>
+        {
+    isCustomerAuthenticated() ?
+    
     <div className='navbar'>
         <div className="wrapper">
             
@@ -15,7 +31,41 @@ const Navbar = () => {
             </div>
             
             <div className='center'>
-                <Link className='link' to="/">EX-ZBT</Link>
+                <Link className='link' to="/home">EX-ZBT</Link>
+            </div>
+
+            <div className='right'>
+
+                <div className="item">
+                    <SwitchAccountIcon />
+                    <Link className='link' to="/profile">{currentUser.username}</Link>
+                </div>
+
+                <div className="item">
+                    <WorkIcon />
+                    <Link className='link' to="/jobs">Jobs</Link>
+                </div>
+                
+        
+                <div className="item">
+                    <LoginIcon/>
+                    
+                    <button onClick={handleLogout}>Logout</button>
+                </div>
+
+            </div>
+        </div>
+    </div> :
+
+    <div className='navbar'>
+        <div className="wrapper">
+            
+            <div className='left'>
+               <img src={logo} alt="logo"></img>
+            </div>
+            
+            <div className='center'>
+                <Link className='link' to="/home">EX-ZBT</Link>
             </div>
 
             <div className='right'>
@@ -33,7 +83,9 @@ const Navbar = () => {
             </div>
         </div>
     </div>
+}
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
