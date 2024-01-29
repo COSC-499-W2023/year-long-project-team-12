380cb90d53622f7144c296bc5e148315ac96e5ed
Hameed <87030215+Abdulhameed23@users.noreply.github.com>
2024-01-28 23:50:30 -0800

    New User APIs for CRUD functionality

--- app/Java/widget/src/main/java/com/exzbt/widget/user/UserCRUD.java
+++ app/Java/widget/src/main/java/com/exzbt/widget/user/UserCRUD.java
@@ -2,32 +2,56 @@ package com.exzbt.widget.user;
 
 import com.exzbt.business.user.shared.UserDetailRequest;
 import com.exzbt.business.user.UserService;
-import com.exzbt.business.user.userRegistration.userRegistrationService;
+import com.exzbt.business.user.shared.UserDetailsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/user")
+@RequestMapping("api/v1/users")
 public class UserCRUD {
-    @Autowired
-    private userRegistrationService registrationService;
     @Autowired
     private UserService userService;
 
+    @GetMapping
+    public List<UserDetailsDTO> getUsers() {
+        return userService.getAllUsers();
+    }
+
     @GetMapping("{id}")
-    public List<UserDetailRequest> findUser(@PathVariable String id) {
-        return userService.findAllUsers();
+    public UserDetailsDTO getUser(@PathVariable("id") String id) {
+        return userService.getUser(id);
     }
 
-    @GetMapping("get/all/1")
-    public List<UserDetailRequest> findAllUsers() {
-        return userService.findAllUsers();
+    @PostMapping("{id}")
+    public UserDetailsDTO saveChanges(@PathVariable("id") String id,
+            @RequestBody UserDetailRequest updateRequest) {
+        return userService.saveChanges(id, updateRequest);
     }
 
-    public Object deleteUser(Object userAttempt) {
-        return null;
+    @DeleteMapping("{id}")
+    public void deleteUser(@PathVariable("id") String id) {
+        userService.deleteUserById(id);
     }
 
+    @PostMapping(
+            value = "{id}/profileImage",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public void updateUserProfileImage(
+            @PathVariable("id") String id,
+            @RequestParam("image") MultipartFile file){
+        userService.uploadUserProfileImage(id, file);
+    }
+
+    @GetMapping(
+            value = "{id}/profileImage",
+            produces = MediaType.IMAGE_JPEG_VALUE
+    )
+    public byte[] getUserProfileImage(@PathVariable("id") String id){
+        return userService.getUserProfileImage(id);
+    }
 }
