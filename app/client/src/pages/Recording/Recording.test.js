2aa8577b5b347fa5395dbbb2dde8e9ad65f2fc94
Hameed <87030215+Abdulhameed23@users.noreply.github.com>
2024-01-30 23:35:40 -0800

    More changes to tests

--- app/client/src/pages/Recording/Recording.test.js
+++ app/client/src/pages/Recording/Recording.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Recording from "./Recording";
 
 jest.mock("axios");
@@ -41,8 +41,4 @@ describe("Recording component", () => {
 //       );
 //     });
 //   });
-
-  
-
- 
 });
