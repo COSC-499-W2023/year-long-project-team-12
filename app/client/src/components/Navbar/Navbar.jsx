c401b5c437276af5e571312c76b2206d9a528af2
Hameed <87030215+Abdulhameed23@users.noreply.github.com>
2024-03-05 11:41:14 -0800

    Navbar Update

--- app/client/src/components/Navbar/Navbar.jsx
+++ app/client/src/components/Navbar/Navbar.jsx
@@ -3,20 +3,19 @@ import { Link, useParams } from 'react-router-dom';
 import LoginIcon from '@mui/icons-material/Login';
 import AddIcon from '@mui/icons-material/Add';
 import SwitchAccountIcon from '@mui/icons-material/SwitchAccount';
-import WorkIcon from '@mui/icons-material/Work';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useAuth } from "../../context/authContext"
 import logo from "./logo.png";
 import { useNavigate} from "react-router-dom";
 import DarkMode from '../DarkMode/DarkMode';
+import {getUserProfileImage} from "../../services/ClientAPI"
 import "./Navbar.scss";
 
 const Navbar = () => {
     const { isCustomerAuthenticated,currentUser, logOut, isHiring} = useAuth();
     let navigate = useNavigate();
     const [showConfirmation, setShowConfirmation] = useState(false);
-
-
 
     const handleLogout = (event) => {
         setShowConfirmation(true);
@@ -31,7 +30,6 @@ const Navbar = () => {
         setShowConfirmation(false);
     }
     
-
     return (
         <div>
             {isCustomerAuthenticated() ?
@@ -40,44 +38,47 @@ const Navbar = () => {
                     <div className="wrapper">
 
                         <div className='left'>
+                            <div className='goBack'><ArrowBackIcon onClick={()=> navigate(-1)} /><ArrowForwardIcon onClick={()=> navigate(1)} /></div>
                             <Link to="/home">
                                 <img src={logo} alt="logo"></img>
                             </Link>
                             <DarkMode />
                         </div>
 
-
-
                         <div className='center'>
-                        <div onClick={()=> navigate(-1)} className='goBack'><ArrowBackIcon /> Go back</div>
                         
                         </div>
 
                         <div className='right'>
-
-                            <div className="item">
-                                <SwitchAccountIcon />
-                                <Link className='link' to="/profile">{currentUser.username}</Link>
-                            </div>
-
-                            {isHiring(currentUser) && (
-                                <div className="item">
-                                    <AddIcon />
-                                    <Link className='link' to="/admin">Add a Request</Link>
+                            <Link className='link' to="/profile">
+                                <div className="item" >
+                                    {currentUser.profileImageId? 
+                                        <img src={getUserProfileImage(currentUser.userId)} alt='profilePic' style={{width:'2em', 
+                                        height:'2em', borderRadius:'50%', marginRight:'2px'
+                                    }}/>
+                                        :
+                                        <SwitchAccountIcon />
+                                    }
+                                    
+                                    {currentUser.username}
                                 </div>
-                            )}
+                            </Link>
+                            
 
-                            <div className="item">
-                                <WorkIcon />
-                                <Link className='link' to="/jobs">Requests</Link>
-                            </div>
+                            
+                                {isHiring(currentUser) && (
+                                    <div className="item">
+                                        <AddIcon />
+                                        <Link className='link' to="/admin">Add a Request</Link>
+                                    </div>
+                                )}
 
+                                <div className="item">
 
-                            <div className="item">
-                                <LoginIcon />
-
-                                <button onClick={handleLogout}>Logout</button>
-                            </div>
+                                    <button onClick={handleLogout}>Logout</button>
+                                </div>
+                            
+                            
 
                         </div>
                     </div>
@@ -118,14 +119,15 @@ const Navbar = () => {
             }
 
             {showConfirmation &&
-                <div className="confirmation-dialog">
-                    <p>Are you sure you want to logout?</p>
+                <div className='modalbackground'>
+                    <div className="confirmation-dialog">
+                        <p>Are you sure you want to logout?</p>
 
-                    <div className="button-container">
-                        <button onClick={confirmLogout}>Yes</button>
-                        <button onClick={cancelLogout}>No</button>
+                        <div className="button-container">
+                            <button onClick={confirmLogout}>Yes</button>
+                            <button onClick={cancelLogout}>No</button>
+                        </div>
                     </div>
-                
                 </div>
             }
 
