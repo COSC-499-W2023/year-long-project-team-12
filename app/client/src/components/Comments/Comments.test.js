2075e1494655ef8f97f6a23e97bba82a5b8424cc
Hameed <87030215+Abdulhameed23@users.noreply.github.com>
2024-04-07 11:02:50 -0700

    Fixed Tests

--- app/client/src/components/Comments/Comments.test.js
+++ app/client/src/components/Comments/Comments.test.js
@@ -2,6 +2,16 @@ import {render,screen} from "@testing-library/react";
 import React from "react";
 import Comments from "./Comments";
 
+jest.mock('../../context/authContext', () => ({
+  useAuth: () => ({
+    currentUser: { role: 'ADMIN' },
+    currentRequest: {
+      title: 'MockTitle', 
+      description: 'MockDescription', 
+      expiration: '05 October 2011 14:48 UTC' 
+    }
+  })  
+}));
 
   test("Comments component is rendered correctly with text-area", () => {
     render(<Comments/>);
