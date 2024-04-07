7f98b1dc5cb893b398a8d936084e1f61656755a3
tmg-repo <tmg.01@icloud.com>
2024-04-07 09:36:24 -0700

    front end changes for password reset

--- app/client/src/pages/Login/Login.jsx
+++ app/client/src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
-import "./Login.scss"
-import { useAuth } from "../../context/authContext"
-import {redirect, useNavigate,Navigate,} from "react-router-dom";
+import "./Login.scss";
 import { useState } from "react";
+import { useAuth } from "../../context/authContext";
+import { useNavigate, Navigate } from "react-router-dom";
 
 const Login = () => {
     const { login, isCustomerAuthenticated } = useAuth();
@@ -10,47 +10,45 @@ const Login = () => {
     const [loginParam, setLoginParam] = useState("");
     const [password, setPassword] = useState("");
     const [show403ErrorPopup, setShow403ErrorPopup] = useState(false);
+    const [forgotPasswordUsername, setForgotPasswordUsername] = useState("");
+    const [showForgotPasswordForm, setShowForgotPasswordForm] = useState(false);
+
     let navigate = useNavigate();
 
-    if(isCustomerAuthenticated()){
-        return <Navigate to="/profile"/>
-      }
+    if (isCustomerAuthenticated()) {
+        return <Navigate to="/profile" />;
+    }
 
     const toggleRegister = () => {
-        navigate("/register")
-    };
-
-    const isValidEmail = (value) => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(value);
+        navigate("/register");
     };
 
     const handleLogin = async (event) => {
         event.preventDefault();
         setLoading(true);
-        setError(false); 
-        setShow403ErrorPopup(false); 
-    
+        setError(false);
+        setShow403ErrorPopup(false);
+
         try {
             const object = {};
             const formData = new FormData();
-    
+
             if (isValidEmail(loginParam)) {
                 formData.append("email", loginParam);
             } else {
                 formData.append("username", loginParam);
             }
-            formData.append("password", password)
+            formData.append("password", password);
 
-            formData.forEach((value, key) => object[key] = value);
-            const loginParamAndPassword = JSON.stringify(object)
-            
-            await login(loginParamAndPassword).then(resp => {
+            formData.forEach((value, key) => (object[key] = value));
+            const loginParamAndPassword = JSON.stringify(object);
+
+            await login(loginParamAndPassword).then((resp) => {
                 navigate("/profile");
             });
         } catch (error) {
             setLoading(false);
-    
+
             if (error.response && error.response.status === 403) {
                 setShow403ErrorPopup(true);
             } else {
@@ -59,44 +57,74 @@ const Login = () => {
         }
     };
 
+    const handleForgotPasswordSubmit = async (event) => {
+        event.preventDefault();
+        
+        console.log("Requesting password reset for:", forgotPasswordUsername);
+        setShowForgotPasswordForm(false); 
+    };
+
+    const isValidEmail = (value) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(value);
+    };
+
     return (
         <div className="login">
             <div className="card">
                 <div className="left">
                     <h1>EX-ZBT</h1>
                     <p>
-                    The purpose of the software is to provide a video sharing platform 
-                     It allows users to register, create profiles, upload and share videos and receive feedback based on their video submissions.
+                        The purpose of the software is to provide a video sharing platform. It allows users to register, create profiles, upload and share videos, and receive feedback based on their video submissions.
                     </p>
                     <button onClick={toggleRegister}>Register</button>
                 </div>
-                <div className="right">
-                    <h1>Login</h1>
-                    <form onSubmit={handleLogin}>
-                        <input type="text" name="loginParam" placeholder="Email or username" value={loginParam} onChange={(e) => setLoginParam(e.target.value)}/>
-                        <input type="password" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                        <button disabled={!loginParam || !password} type="submit">{loading ? "Please wait" : "Login"}</button>
-                        <span
-                         data-testid="error"
-                         style={{visibility:error? "visible":"hidden"}}
-                        >Something went wrong!
-                        </span>
-                    </form>
+                {!showForgotPasswordForm && (
+                    <div className="right">
+                        <h1>Login</h1>
+                        <form onSubmit={handleLogin}>
+                            <input type="text" name="loginParam" placeholder="Email or username" value={loginParam} onChange={(e) => setLoginParam(e.target.value)} />
+                            <input type="password" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                            <button disabled={!loginParam || !password} type="submit">
+                                {loading ? "Please wait..." : "Login"}
+                            </button>
+                            <span data-testid="error" style={{ visibility: error ? "visible" : "hidden" }}>
+                                Something went wrong!
+                            </span>
+                        </form>
+                        <a href="#!" className="forgot-password-link" onClick={() => setShowForgotPasswordForm(true)}>
+                            Forgot your password?
+                        </a>
+                    </div>
+                )}
 
-                </div>
+                {showForgotPasswordForm && (
+                    <div className="right">
+                        <h2>Reset Password</h2>
+                        <form onSubmit={handleForgotPasswordSubmit}>
+                            <input
+                                type="text"
+                                placeholder="Email or username"
+                                value={forgotPasswordUsername}
+                                onChange={(e) => setForgotPasswordUsername(e.target.value)}
+                            />
+                            <button type="submit">Submit</button>
+                        </form>
+                        <a href="#!" className="back-arrow" onClick={() => setShowForgotPasswordForm(false)}>
+                            ← Back
+                        </a>
+                    </div>
+                )}
             </div>
-            
+
             {show403ErrorPopup && (
                 <div className="error-popup">
-                    <p>Incorrect login details, ensure your username/email and password are entered correctly. </p>
+                    <p>Incorrect login details, ensure your username/email and password are entered correctly.</p>
                     <button onClick={() => setShow403ErrorPopup(false)}>Close</button>
                 </div>
             )}
-
         </div>
-
-        
-    )
-}
+    );
+};
 
 export default Login;
