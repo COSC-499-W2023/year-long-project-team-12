983ecb7741ddd7e64960bcd69c3d838652e98796
Hameed <87030215+Abdulhameed23@users.noreply.github.com>
2024-04-07 19:38:35 -0700

    Fix

--- app/client/src/pages/Profile/Profile.jsx
+++ app/client/src/pages/Profile/Profile.jsx
@@ -1,3 +1,4 @@
+
 import "./profile.scss";
 import PlaceIcon from "@mui/icons-material/Place";
 import LanguageIcon from "@mui/icons-material/Language";
@@ -74,22 +75,22 @@ const Profile = () => {
 
   const dropdownSettings = () => {
     setShowSettings(!showSettings);
-  };
-
+  }
+  
   return (
     <div className="profile">
       <div className="images">
         <img
           src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="Profile background"
+          alt=""
           className="cover"
         />
       </div>
       <div className="profileContainer">
-        <div className="uInfo">
+        <div className="uInfo">          
           <div className="center">
           <ImageCropProvider>
-            <ProfilePicChanger />
+          <ProfilePicChanger />
           </ImageCropProvider>
           
             
@@ -106,8 +107,9 @@ const Profile = () => {
                 <span>{currentUser.email}</span>
               </div>
             </div>
-            <button onClick={handleMyRequestsClick} className="myRequestsButton">View My Requests</button>  
-            <button onClick={handleRecordedVideosClick} className="recordedVideosButton">Saved Videos</button>
+            
+            <button onClick={handleMyRequestsClick} className="myRequestsButton"> {showRequests ? 'Hide My Requests' : 'View My Requests'} </button>  
+            <button onClick={handleRecordedVideosClick} className="recordedVideosButton">{showVideos ? 'Hide My Videos' : "Saved Videos"} </button>
             <button onClick={handleNotificationsClick} className="notificationsButton">
             <NotificationsIcon onClick={toggleDrawer(true)} fontSize="large"
              className="notificationsButton"/>
@@ -115,23 +117,22 @@ const Profile = () => {
               <span className="notificationCount">{notificationCount}</span>
             )}
             </button>
-            <button onClick={dropdownSettings} className="settingsbtn">
-              <SettingsIcon fontSize="large" />
-            </button>
-            {showSettings && (
-              <div className="dropdown">
-                <div className="dropdown-content">
+          <button onClick={dropdownSettings} className="settingsbtn">
+                   <SettingsIcon fontSize="large"
+                   className="settingsbtn"/> 
+          </button>
+
+          {showSettings && 
+            <div className = "dropdown">
+                <div id="myDropdown" className="dropdown-content">
                   <Link to="/changename">Change Name</Link>
                   <Link to="/changepassword">Change Password</Link>
                   <Link to="/contactUs">Give Feedback</Link>
                 </div>
-              </div>
-            )}
+            </div>
+          }
           </div>
         </div>
-        {showRequests && <Posts displayLimit={5} />}
-        {showVideos && <Videos />}
-      </div>
 
         
         {showRequests && (
@@ -161,10 +162,9 @@ const Profile = () => {
                 {!clearAll? <NotificationDrawer /> : <>No Notifications!</>}
             </div>
         </Drawer>
+      </div>
     </div>
   );
 };
 
 export default Profile;
-
-
