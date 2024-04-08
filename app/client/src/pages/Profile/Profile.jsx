defc17a32d39b9d08db9e32d376d1d5b802f04cb
Hameed <87030215+Abdulhameed23@users.noreply.github.com>
2024-04-07 19:23:45 -0700

    Changes

--- app/client/src/pages/Profile/Profile.jsx
+++ app/client/src/pages/Profile/Profile.jsx
@@ -1,30 +1,51 @@
-import React, { useState } from 'react';
-import './profile.scss';
-import PlaceIcon from '@mui/icons-material/Place';
-import LanguageIcon from '@mui/icons-material/Language';
+import "./profile.scss";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import Drawer from '@mui/material/Drawer';
+import {useAuth} from "../../context/authContext";
+import { useState,useEffect } from "react";
+import { Link } from 'react-router-dom';
 import SettingsIcon from '@mui/icons-material/Settings';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { useAuth } from '../../context/authContext';
-import { Link } from 'react-router-dom';
-import ProfilePicChanger from './profilepicchanger.js';
-import Posts from '../../components/Posts/Posts.jsx';
-import Videos from '../../components/Videos/Videos.jsx';
+import ProfilePicChanger from "./profilepicchanger.jsx";
+import Posts from "../../components/Posts/Posts.jsx";
+import Videos from "../../components/Videos/Videos.jsx";
+import ImageCropProvider from "./ImageCropProvider.jsx";
+
+import NotificationDrawer from "../../components/Notification/NotificationDrawer.jsx";
+import { getNotifications, deleteAllNotifications, updateNotifications } from '../../services/ClientAPI';
 
 const Profile = () => {
   const { currentUser } = useAuth();
   const [showRequests, setShowRequests] = useState(false);
   const [showVideos, setShowVideos] = useState(false);
   const [showSettings, setShowSettings] = useState(false);
-  const [notificationCount, setNotificationCount] = useState(2);
-  const [notificationSidebarVisible, setNotificationSidebarVisible] = useState(false);
+  const [notificationCount, setNotificationCount] = useState(0);
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [notifications, setMyNotifications] = useState([]);
+  const [clearAll, setClear] = useState(false);
 
-  const notifications = [
-    'Notification 1',
-    'Notification 2',
-    'Notification 3',
-    // ... additional notifications as needed
-  ];
+  const getMyNotifications = () => {
+    getNotifications(currentUser.userId)
+      .then((resp) => {
+        let sorted = resp.data.sort((a,b) => b.created - a.created)
+        .filter((notification) => !notification.viewed);
+        setMyNotifications(sorted);
+        setNotificationCount(sorted.length);
+      })
+      .catch((err) => {});
+  };
 
+  useEffect(() => {
+    getMyNotifications();
+  }, []);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpenDrawer(newOpen);
+    updateNotifications(currentUser.userId);
+  };
+ 
+  
   const handleMyRequestsClick = () => {
     setShowRequests(!showRequests);
     setShowVideos(false);
@@ -36,7 +57,19 @@ const Profile = () => {
   };
 
   const handleNotificationsClick = () => {
-    setNotificationSidebarVisible(!notificationSidebarVisible);
+    setNotificationCount(0);
+  };
+
+  const handleClear = (event) => {
+    event.preventDefault();
+    setClear(true);
+
+    deleteAllNotifications(currentUser.userId).then(response => {
+      getNotifications(currentUser.userId).then((resp) => {
+        setMyNotifications(resp.data);
+        setNotificationCount(resp.data.length);
+      })
+    }).catch((err) => {});
   };
 
   const dropdownSettings = () => {
@@ -55,7 +88,13 @@ const Profile = () => {
       <div className="profileContainer">
         <div className="uInfo">
           <div className="center">
+          <ImageCropProvider>
             <ProfilePicChanger />
+          </ImageCropProvider>
+          
+            
+           
+          
             <span>{currentUser.firstname} {currentUser.lastname}</span>
             <div className="info">
               <div className="item">
@@ -70,8 +109,11 @@ const Profile = () => {
             <button onClick={handleMyRequestsClick} className="myRequestsButton">View My Requests</button>  
             <button onClick={handleRecordedVideosClick} className="recordedVideosButton">Saved Videos</button>
             <button onClick={handleNotificationsClick} className="notificationsButton">
-              <NotificationsIcon fontSize="large" />
-              {notificationCount > 0 && <span className="notificationCount">{notificationCount}</span>}
+            <NotificationsIcon onClick={toggleDrawer(true)} fontSize="large"
+             className="notificationsButton"/>
+            {notificationCount > 0 && (
+              <span className="notificationCount">{notificationCount}</span>
+            )}
             </button>
             <button onClick={dropdownSettings} className="settingsbtn">
               <SettingsIcon fontSize="large" />
@@ -80,7 +122,6 @@ const Profile = () => {
               <div className="dropdown">
                 <div className="dropdown-content">
                   <Link to="/changename">Change Name</Link>
-                  <a href="#">Change Background photo</a>
                   <Link to="/changepassword">Change Password</Link>
                   <Link to="/contactUs">Give Feedback</Link>
                 </div>
@@ -92,12 +133,34 @@ const Profile = () => {
         {showVideos && <Videos />}
       </div>
 
-      {/* Notification Sidebar! */}
-      <div className={`notificationSidebar ${notificationSidebarVisible ? 'visible' : ''}`}>
-        {notifications.map((notification, index) => (
-          <div key={index} className="notificationItem">{notification}</div>
-        ))}
-      </div>
+        
+        {showRequests && (
+              <div className="collapsibleContainer">
+                <h2>My Requests</h2>
+                <div className="collapsibleLists">
+                  <Posts displayLimit={5} />
+                </div>
+              </div>
+        )}
+
+        {showVideos && (
+              <div className="collapsibleContainer">
+                <h2>My Videos</h2>
+                <div className="collapsibleLists">
+                  <Videos />
+                </div>
+              </div>
+        )}
+
+        <Drawer anchor={'right'} open={openDrawer} onClose={toggleDrawer(false)}>
+            <div className="notifDrawer"> 
+              <div className="headingNotif">
+                <h3>Notifications</h3>
+                {(notifications.length > 0 && !clearAll) && <h4 onClick={handleClear}>CLEAR ALL</h4>}
+              </div>  
+                {!clearAll? <NotificationDrawer /> : <>No Notifications!</>}
+            </div>
+        </Drawer>
     </div>
   );
 };
